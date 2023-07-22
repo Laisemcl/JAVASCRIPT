93b04a64a033a43ec105f3eed7e741cb948a180c
Laisemcl <laisemclhomeoffice@yahoo.com>
2023-07-21 21:27:22 -0300

    aula javascript erico

---
+++ operadores.js
@@ -0,0 +1,36 @@
+//Aritméticos
+// + adição
+// - subtração
+// * multiplicação
+// / divisão
+
+let numeroUm = 5
+let numeroDois = 6
+let numeroTres = -1
+console.log(numeroDois + numeroTres)
+
+
+//relacionais
+// >  maior que
+// <  menor que
+// != diferente
+// !== compara o tipo
+// ==   igual
+// === compara o tipo
+// >= maior ou igual
+// <= menor ou igual
+
+
+let numeroString = "6"
+console.log(numeroDois === numeroString)
+
+// = é uma atribuição
+
+//Lógicos
+// && E
+// ||OU
+// ! NÃO
+
+console.log(!(numeroDois === numeroString))
+
+
---
+++ variaveis.js
@@ -0,0 +1,14 @@
+//usando var ou nada
+//    identificadordor
+//        atribuição
+//            valor
+var nome="erico";
+sobrenome="costa";
+console.log(nome)
+
+//não permite a execução antes da criação (ERRO)
+let nome2= "Senac"
+console.log(typeof nome2)
+
+const pi=3.141923
+console.log(typeof pi)
