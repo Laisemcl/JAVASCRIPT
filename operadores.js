b5b70f6d0e73861ec94459836c51104f0858793d
Laisemcl <laisemclhomeoffice@yahoo.com>
2023-07-24 20:56:37 -0300

    atualização javascript

--- operadores.js
+++ operadores.js
@@ -3,12 +3,16 @@
 // - subtração
 // * multiplicação
 // / divisão
+// % modulo (resto)
 
 let numeroUm = 5
 let numeroDois = 6
 let numeroTres = -1
-console.log(numeroDois + numeroTres)
+console.log("NumeroDois ("+numeroDois+") + numeroTres ("+ numeroTres+") = "+(numeroDois+numeroTres));
+console.log (numeroDois+"+" +numeroTres+"=" +(numeroDois+numeroTres));
 
+console.log(numeroDois%numeroUm);
+ console.log(`NumeroDois (${numeroDois})+ numeroTres (${numeroTres})= ${numeroDois+numeroTres}`)
 
 //relacionais
 // >  maior que
@@ -33,4 +37,31 @@ console.log(numeroDois === numeroString)
 
 console.log(!(numeroDois === numeroString))
 
+// autoincremento 
+let a=0;
+a = a + 1;
+console.log(a);
+
+a++;
+console.log(a);
+
+++a;
+console.log(a);
+
+let b = 10;
+console.log(b--);
+
+console.log(--b);
+
+//aritméticos de atribuição
+// +=
+// -=
+// *=
+// /=
+// %=
+
+let c = 0;
+c = c + 5;
+c += 5;
+console.log(c);
 
